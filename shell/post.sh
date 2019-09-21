45c579ba6f1e1fb2337ab918bd7e81f634ae4bbb
zhoushuai <zhoushuai@luojilab.com>
2019-09-21 16:24:59 +0800

    check file status and format

--- shell/post.sh
+++ shell/post.sh
@@ -1,15 +1,23 @@
 #! /bin/bash
-count=$(cat test.json | wc -l)
+filename=test.json # can be from args 
+if [ ! -f "$filename" ]; then
+    echo "no such file:"$filename
+    exit 1
+fi
+count=$(cat $filename | wc -l)
+if [ $count -eq 0 ]; then
+    echo "file count is zero"
+    exit 1
+fi
 # for((i=1;i<=count;i++));
-for i in $(seq 1 $count)
-do
-data=`head -n 1 test.json| jq -r '.body'`
-echo $data
-echo "count":$i 
-curl -X POST \
-http://127.0.0.1:8080/v1/api/ \
--H 'Content-Type: application/json' \
--d "$data"
-sleep 0.5 # sleep 0.5s
-sed -i "" '1d' test.json #delete this raw
+for i in $(seq 1 $count); do
+    data=$(head -n 1 $filename | jq -r '.body')
+    echo $data
+    echo "count":$i
+    curl -X POST \
+        http://127.0.0.1:8080/v1/api/ \
+        -H 'Content-Type: application/json' \
+        -d "$data"
+    sleep 0.5                # sleep 0.5s
+    sed -i "" '1d' $filename #delete this raw
 done
